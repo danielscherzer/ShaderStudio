017108fdcfb7e11ea7335bf988fde0424bd74386
scherzer <scherzer@hs-weingarten.de>
2017-12-28 22:48:18 +0100

    ShaderViewPanelTool added icon

--- src/ShaderViewPanelTool/ShaderViewPanelCommandDefinition.cs
+++ src/ShaderViewPanelTool/ShaderViewPanelCommandDefinition.cs
@@ -8,18 +8,13 @@ namespace ShaderStudio.ShaderViewPanelTool
 	[CommandDefinition]
 	public class ShaderViewPanelCommandDefinition : CommandDefinition
 	{
-		public const string CommandName = "View.ShaderViewPanel";
-
-		public override string Name => CommandName;
+		public override string Name => "View.ShaderViewPanel";
 
 		public override string Text => "Shader View Panel";
 
 		public override string ToolTip => Text;
 
-		public override Uri IconSource
-		{
-			get { return new Uri("pack://application:,,,/Gemini;component/Resources/Icons/Undo.png"); } //TODO: add icon
-		}
+		public override Uri IconSource => new Uri("/Resources/DynamicImage.png", UriKind.Relative);
 
 		[Export]
 		public static CommandKeyboardShortcut KeyGesture = new CommandKeyboardShortcut<ShaderViewPanelCommandDefinition>(new KeyGesture(Key.V, ModifierKeys.Control));
